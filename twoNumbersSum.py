f8b30a5611ec2e2dce30699cbf258c3c1686a472
trofiv <vladislav_trofimov@homedepot.com>
2020-04-02 12:54:24 +0300

    two numbers sum

---
+++ twoNumbersSum.py
@@ -0,0 +1,20 @@
+#
+#   Two Number Sum
+#
+#  Write a function that takes in a non-empty array of distinct integers
+#  and an integer representing a target sum. If any two numbers in the
+#  input array sumup to the target sum, the function should return them
+#  in an array, in any order. If no two numbers sum up to the target
+#  sum, the function should return an empty array.
+#
+
+
+def twoNumberSum(array, targetSum):
+    sums = set()
+    for i in array:
+        left = targetSum - i
+        if left in sums:
+            return [i, left]
+        else:
+            sums.add(i)
+    return []
